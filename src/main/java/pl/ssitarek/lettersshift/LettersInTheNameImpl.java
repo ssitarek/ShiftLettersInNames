3de2df7a1dc02c764ecf1712e3eb4b595d4873ff
Stefan Sitarek <s.sitarek@cinema-city.pl>
2018-07-12 14:20:27 +0200

    add method body

--- src/main/java/pl/ssitarek/lettersshift/LettersInTheNameImpl.java
+++ src/main/java/pl/ssitarek/lettersshift/LettersInTheNameImpl.java
@@ -13,6 +13,30 @@ public class LettersInTheNameImpl implements LettersInTheName {
     @Override
     public int calculateNumberOfShift() {
 
-        return -1;
+        name1 = name1.toLowerCase();
+        name2 = name2.toLowerCase();
+        System.out.println(name2);
+        int countShift = 0;
+        for (int i = 0; i < name1.length(); i++) {
+            countShift += moveSingleLetterInSecondName(i);
+        }
+        return countShift;
+    }
+
+    private int moveSingleLetterInSecondName(int i) {
+
+        Character letterAnalyzed = name1.charAt(i);
+        Character letterName2 = name2.charAt(i);
+        if (letterName2.equals(letterAnalyzed)) {
+            return 0;
+        }
+
+        int letterIndex = name2.indexOf(letterAnalyzed, i);
+        String fixedPart = name2.substring(0, i);
+        String beforeNextApperance = name2.substring(i, letterIndex);
+        String afterNextApperance = name2.substring(letterIndex + 1, name2.length());
+        name2 = fixedPart + letterAnalyzed + beforeNextApperance + afterNextApperance;
+        System.out.println(name2 + " " + beforeNextApperance.length());
+        return beforeNextApperance.length();
     }
 }
